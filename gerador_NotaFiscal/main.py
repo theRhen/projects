7d0d34f3c834d06ec8232b6379e387de61daedf2
riquelme <164274153+theRhen@users.noreply.github.com>
2025-08-25 15:24:08 -0300

    Update main.py

--- gerador_NotaFiscal/main.py
+++ gerador_NotaFiscal/main.py
@@ -1,9 +1,16 @@
 from func_maquininha import maquininha
+from datetime import datetime
 
 loja = input("Loja: ").upper() # loja(variable) recebe entrada.maiúsculo
-descricao = input("Descrição: ").lower() # descricao(variable) recebe entrada.minúsculo
+descricao = input("Descrição: ").strip().title() # descricao(variable) recebe entrada.minúsculo
 valor_str = maquininha()  # valor_str(variable) recebe maquininha(função)
-data = input("\nData: ") # data(variable) recebe entrada
+while True: # laço while para validar a data (só sai quando for digitada corretamente)
+    data_input = input("\nData (DD/MM/AAAA): ").strip()
+    try:
+        data = datetime.strptime(data_input, "%d/%m/%Y").date()
+        break
+    except ValueError:
+        print("⚠️ Data inválida! Digite no formato DD/MM/AAAA.") 
 
 print("-----------------------------------")
 print("|           NOTA FISCAL           |")
@@ -11,7 +18,7 @@ print("-----------------------------------")
 print(f"Loja: *{loja}*")
 print(f"Descrição: {descricao}")
 print(f"Valor: {valor_str}")
-print(f"Data: {data}")
+print(f"Data: {data.strftime('%d/%m/%Y')}")
 print("-----------------------------------")
 print("|  compra cadastrada com sucesso  |")
 print("-----------------------------------")
