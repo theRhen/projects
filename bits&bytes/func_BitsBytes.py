4751d6c5b0b19683b2164a267818a54e7398fbd7
riquelme <164274153+theRhen@users.noreply.github.com>
2025-10-19 20:16:23 -0300

    Create func_BitsBytes.py

---
+++ bits&bytes/func_BitsBytes.py
@@ -0,0 +1,14 @@
+#CONVERTER BITS PARA BYTES
+def bits_para_bytes(bits):
+    return bits / 8
+
+#CONVERTER BYTES PARA BITS
+def bytes_para_bites(bytes):
+    return bytes * 8
+
+#ENTRADAS
+bits = int(input("BITS → BYTES: "))
+print(bits, "bits", "=", bits_para_bytes(bits), "bytes")
+
+bytes = int(input("BYTES → BITS: "))
+print(bytes, "bytes", "=", bytes_para_bites(bytes), "bits")
