9f9fdf8d58111b292998394c1cc620d04d9ce3cf
riquelme <164274153+theRhen@users.noreply.github.com>
2025-12-08 02:48:15 -0300

    Refactor comments and improve readability in ServidorTCP.py

--- FileServerTCP/ServidorTCP.py
+++ FileServerTCP/ServidorTCP.py
@@ -1,13 +1,13 @@
 import socket
 import os
 
-HOST = '192.168.56.1' # IP do servidor (IP do HOST rodando o servidor)
+HOST = '192.168.56.1' # IP do servidor (IP do HOST servidor)
 PORT = 20000          # Definindo a porta     
 
 # Criando o socket TCP
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_socket.bind((HOST, PORT))
-tcp_socket.listen(1) # Máximo de conexões enfileiradas
+tcp_socket.bind((HOST, PORT)) # Ligando o socket a porta
+tcp_socket.listen(1)          # Máximo de conexões enfileiradas
 
 print("------------------------------------------------------")
 print("|  Servidor TCP de arquivos iniciado na porta 20000  |")
@@ -18,35 +18,35 @@ while True:
     print("Aguardando arquivo...\n")
     con, endereco_cliente = tcp_socket.accept() # Aceita a conexão com o cliente
     
-    # Primeiro Datagrama (Tamanho do nome do arquivo ⭢ 1 Byte)
-    primeiro_data = con.recv(1) # Servidor espera receber 1 Byte (Primeiro Datagrama)
-    tamanho_nome = int.from_bytes(primeiro_data, "big") # Converte o 1 Byte recebido em um inteiro.
+    # Datagrama 1 (Tamanho do nome do arquivo 1 BYTE)
+    primeiro_data = con.recv(1) # SERVIDOR RECEBENDO 1 Byte (Primeiro Datagrama)
+    tamanho_nome = int.from_bytes(primeiro_data, "big") # INTEIRO (Inteiro ⭠ Bytes)
 
-    # Segundo Datagrama (Nome do arquivo sendo solicitado. ⭢ Vários Bytes)
-    segundo_data = con.recv(tamanho_nome) # Serviodor espera receber vários Byte (Segundo Datagrama)
-    nome_arquivo = segundo_data.decode("utf-8") # Converte vários Bytes recebidos em uma string.
+    # Datagrama 2 (Nome do arquivo VÁRIOS BYTES)
+    segundo_data = con.recv(tamanho_nome) # SERVIDOR RECEBENDO vários Bytes (Segundo Datagrama)
+    nome_arquivo = segundo_data.decode("utf-8") # STRING (String ⭠ Bytes)
 
     print(f"Cliente: {endereco_cliente} | Solicitou: {nome_arquivo}\n")
 
     # Caminho do arquivo (files/"nome do arquivo dado pelo usuário")
-    caminho_arquivo = os.path.join("files", nome_arquivo) # nome_arquivo = vários bytes convertidos em string
+    caminho_arquivo = os.path.join("files", nome_arquivo) 
 
     # Verifica se o arquivo existe
-    if not os.path.exists(caminho_arquivo):
+    if not os.path.exists(caminho_arquivo): 
         print("Arquivo não encontrado.\n")
-        con.sendall(b'\x00')  # Envia 0 → Arquivo não existe
+        con.sendall(b'\x00') # SERVIDOR ENVIANDO 0 → Arquivo não existe
         print("------------------------------------------------------\n")
        
     else: 
         print("Arquivo encontrado. Enviando...") 
-        con.sendall(b'\x01') # Envia 1 → arquivo existe
+        con.sendall(b'\x01') # SERVIDOR ENVIANDO 1 → Arquivo existe
 
         # Obtém tamanho do arquivo
         tamanho_arquivo = os.path.getsize(caminho_arquivo)
     
         # Envia tamanho do arquivo (4 bytes - big-endian)
         tamanho_bytes = tamanho_arquivo.to_bytes(4, byteorder="big")
-        con.sendall(tamanho_bytes)
+        con.sendall(tamanho_bytes) # SERVIDOR ENVIANDO 4 Bytes
 
         # Envia o conteudos em blocos de 4096
         with open(caminho_arquivo, "rb") as f:
