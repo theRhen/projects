fd95ecf40facdd6ee939274853243481daf5fa04
riquelme <164274153+theRhen@users.noreply.github.com>
2025-12-15 15:13:17 -0300

    Update ServidorTCP.py

--- FileServerTCP/ServidorTCP.py
+++ FileServerTCP/ServidorTCP.py
@@ -5,14 +5,14 @@ import json
 HOST = '192.168.56.1' # IP do servidor (IP do HOST servidor)
 PORT = 20000          # Definindo a porta     
 
-# Obtém o diretório absoluto onde o script do servidor está localizado
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-FILES_DIR = os.path.join(BASE_DIR, "files")
+# Obtém o diretório absoluto onde do codigo
+BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # Pasta onde o código esta rodando
+FILES_DIR = os.path.join(BASE_DIR, "files")           # Pasta "files" dentro da pasta onde o código esta rodando
 
 # Criando o socket TCP
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_socket.bind((HOST, PORT)) # Ligando o socket a porta
-tcp_socket.listen(1)          # Máximo de conexões enfileiradas
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Socket TCP (STREAM)
+tcp_socket.bind((HOST, PORT))                                  # Ligando o socket a porta
+tcp_socket.listen(1)                                           # Máximo de conexões enfileiradas
 
 print("------------------------------------------------------")
 print("|  Servidor TCP de arquivos iniciado na porta 20000  |")
@@ -20,10 +20,10 @@ print("------------------------------------------------------\n")
 
 # LOOP PRINCIPAL
 while True:
-    print("Aguardando operação...\n")
+    print("Aguardando arquivo...\n")
     con, endereco_cliente = tcp_socket.accept() # Aceita a conexão com o cliente
     
-    # Operação Datagrama 
+    # Datagrama operação
     operacao_bytes = con.recv(1) # SERVIDOR RECEBENDO 1 Byte (Operação Datagrama)
     operacao = int.from_bytes(operacao_bytes, "big") # Inteiro (Inteiro ⭠ Bytes)
     
@@ -76,38 +76,32 @@ while True:
     elif operacao == 20:
         print(f"Cliente: {endereco_cliente} | Solicitou listagem de arquivos\n")
 
-        try:
-            lista_arquivos = []
+        lista_arquivos = []
 
-            for nome in os.listdir(FILES_DIR):
-                caminho = os.path.join(FILES_DIR, nome)
+        for nome in os.listdir(FILES_DIR):
+            caminho = os.path.join(FILES_DIR, nome)
 
-                if os.path.isfile(caminho):
-                    tamanho = os.path.getsize(caminho)
-                    lista_arquivos.append({
-                        "nome": nome,
-                        "tamanho": tamanho
-                    })
-                    
-            # Converte lsta para json
-            json_lista = json.dumps(lista_arquivos) # JSON (Json(String) ⭠ Lista)
-            json_bytes = json_lista.encode("utf-8") # BYTES (Bytes ⭠ Json(String))
+            if os.path.isfile(caminho):
+                tamanho = os.path.getsize(caminho)
+                lista_arquivos.append({
+                    "nome": nome,
+                    "tamanho": tamanho
+                })
+                
+        # Converte lsta para json
+        json_lista = json.dumps(lista_arquivos) # JSON (Json(String) ⭠ Lista)
+        json_bytes = json_lista.encode("utf-8") # BYTES (Bytes ⭠ Json(String))
 
-            con.sendall(b'\x01') # SERVIDOR ENVIANDO 1 → Listagem será enviada
-            
-            # Calcula o tamanho da lista em bytes "Percorre cada caracter"
-            tamanho_json = len(json_bytes) # INTEIRO (Inteiro ⭠ Bytes) 
-            json_to_bytes = tamanho_json.to_bytes(4, "big") # BYTES (Bytes ⭠ Inteiro)
-            con.sendall(json_to_bytes) # SERVIDOR ENVIANDO 4 Bytes
-
-            con.sendall(json_bytes) # SERVIDOR ENVIANDO Listagem em formato json
-
-            print("Listagem enviada com sucesso.\n")
-            print("------------------------------------------------------\n")
-            
-        except Exception as e:
-            # Status = 0 (erro)
-            con.sendall(b'\x00')
-            print("Erro ao gerar listagem:", e)
+        con.sendall(b'\x01') # SERVIDOR ENVIANDO 1 → Listagem será enviada
         
+        # Calcula o tamanho da lista em bytes "Percorre cada caracter"
+        tamanho_json = len(json_bytes) # INTEIRO (Inteiro ⭠ Bytes) 
+        json_to_bytes = tamanho_json.to_bytes(4, "big") # BYTES (Bytes ⭠ Inteiro)
+        con.sendall(json_to_bytes) # SERVIDOR ENVIANDO 4 Bytes
+
+        con.sendall(json_bytes) # SERVIDOR ENVIANDO Listagem em formato json
+
+        print("Listagem enviada com sucesso.\n")
+        print("------------------------------------------------------\n")
+                
     con.close()
