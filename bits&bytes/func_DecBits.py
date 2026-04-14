524d1e9aadf51853611cc91c3867df5ed696693d
riquelme <164274153+theRhen@users.noreply.github.com>
2026-04-14 19:44:02 -0300

    Delete bits&bytes directory

--- bits&bytes/func_DecBits.py
+++
@@ -1,7 +0,0 @@
-#CONVERTER DECIMAL PARA BITS
-def decimal_para_bits(decimal):
-    return bin(decimal)
-
-#ENTRADA
-decimal = int(input("DEC → BIT: "))
-print(decimal, "(10)", "=", decimal_para_bits(decimal), "(2)")
